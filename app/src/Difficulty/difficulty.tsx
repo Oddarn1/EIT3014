55766be5867093d841d779bca39e8a9ec37e4b44
Odd Andre Owren <oddandreowren@gmail.com>
2021-03-03 15:34:19 +0100

    add links to different difficultis

--- app/src/Difficulty/difficulty.tsx
+++ app/src/Difficulty/difficulty.tsx
@@ -5,10 +5,17 @@ import {
 
 const Difficulty = () => {
     return (
-        <div>
-            Difficulty
-            <Link to={"/location/1?difficulty=3"}>Test</Link>
-        </div>
+        <p>
+            Velg vanskelighetsgrad
+            <br/>
+            <Link to={"/location/1?difficulty=1"}>Ny i byen</Link>
+            <br/>
+            <Link to={"/location/1?difficulty=2"}>Bodd her noen år</Link>
+            <br/>
+            <Link to={"/location/1?difficulty=3"}>Glad i karsk</Link>
+            <br/>
+            <Link to={"/location/1?difficulty=4"}>Stolt eier av skinnvest</Link>
+        </p>
     )
 }
 
